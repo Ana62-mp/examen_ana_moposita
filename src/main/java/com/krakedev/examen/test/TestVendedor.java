3c8822be9863dc2a68a7401316f9e45d6bf72fce
Ana62-mp <ana.moposita84@gmail.com>
2026-05-09 20:05:49 -0500

    APMP: Completar parte 1 con Vendedor, herencia y pruebas unitarias

---
+++ src/main/java/com/krakedev/examen/test/TestVendedor.java
@@ -0,0 +1,16 @@
+package com.krakedev.examen.test;
+
+import com.krakedev.examen.entidades.Vendedor;
+
+public class TestVendedor {
+
+	public static void main(String[] args) {
+		Vendedor vendedor = new Vendedor("1723456789");
+
+		vendedor.setNumeroVentas(5);
+		vendedor.setSueldoFijo(500.0);
+		vendedor.setComisionPorVenta(20.0);
+
+		System.out.println(vendedor);
+	}
+}
---
+++ src/test/java/com/krakedev/examen/VendedorComisionTest.java
@@ -0,0 +1,24 @@
+package com.krakedev.examen;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.krakedev.examen.entidades.VendedorComision;
+
+public class VendedorComisionTest {
+
+	@Test
+	public void validarCalcularSueldoVendedorComision() {
+		// Arrange
+		VendedorComision vendedor = new VendedorComision("1723456789");
+		vendedor.setNumeroVentas(5);
+		vendedor.setComisionPorVenta(20.0);
+		
+		// Act
+		double sueldoCalculado = vendedor.calcularSueldo();
+		
+		// Assert
+		assertEquals(100.0, sueldoCalculado, 0.001);
+	}
+}
